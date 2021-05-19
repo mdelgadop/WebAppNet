ab1bef1371f177938cbf4dc334e08ec73e32af4b
Mario <39182433+mdelgadop@users.noreply.github.com>
2021-05-19 13:59:30 +0200

    v0.21.05.19

--- WebAppCB.Infrastructure/Helpers/DistanciaHelper.cs
+++ WebAppCB.Infrastructure/Helpers/DistanciaHelper.cs
@@ -4,12 +4,15 @@ namespace Infrastructure.Helpers
     {
         public double Distancia(int x1, int y1, int x2, int y2)
         {
-            double dx1 = x1/100000;
-            double dx2 = x2/100000;
-            double dy1 = y1/100000;
-            double dy2 = y2/100000;
+            double dx1 = (double)(((double)x1) / ((double)100000));
+            double dx2 = (double)(((double)x2) / ((double)100000));
+            double dy1 = (double)(((double)y1) / ((double)100000));
+            double dy2 = (double)(((double)y2) / ((double)100000));
 
-            return System.Math.Sqrt(System.Math.Pow((dx1 - dx2), 2) + System.Math.Pow((dy1 - dy2), 2));
+            double distancia = System.Math.Sqrt(System.Math.Pow((dx1 - dx2), 2) + System.Math.Pow((dy1 - dy2), 2));
+            double distancia2 = System.Math.Sqrt(((dx1 - dx2) * (dx1 - dx2)) + ((dy1 - dy2) * (dy1 - dy2)));
+
+            return distancia;
         }
     }
 }
